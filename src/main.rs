b9b7a6e6f5f8837a57cbd01d802b4db8250c6653
Huterok <elllisey@mail.ru>
2023-02-11 00:35:12 +0300

    working version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::stdin;
+
+use primitive_calculator::{Calculator, Error};
+
+fn main() -> Result<(), Error> {
+    loop {
+        let mut input = String::new();
+        match stdin().read_line(&mut input) {
+            Ok(_) => {
+                let tokens = Calculator::parse(input);
+                if tokens.is_err() {
+                    println!("{:?}", tokens.err().unwrap());
+                    continue;
+                }
+                let expr = Calculator::expression(tokens.unwrap());
+                if let Some(v) = Calculator::evaluate(expr) {
+                    println!("{v}")
+                }
+            }
+            Err(err) => println!("{err}"),
+        }
+    }
 }
